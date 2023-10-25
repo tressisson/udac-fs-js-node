386de694c18168dbda8641ef39de51dd4345a1ff
Tres Sisson <tressisson@gmail.com>
2023-10-25 16:28:13 -0400

    basic node server working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('whatever');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// create an express server
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}
+);
+
+
+
+
+
+
+
+
+
+
+// ./node_modules/.bin/eslint yourfile.js
